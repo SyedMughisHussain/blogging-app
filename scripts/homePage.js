0380582e84736bb5f3ae8b2ad55690b57f12c5ba
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-24 09:53:08 +0500

    Blog app has been completed

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,17 +1,17 @@
 import { auth, db } from "../config.js";
 import {
-    collection,
-    getDocs,
-  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const blogs_container = document.querySelector(".blogs-container");
 
 let blogs = [];
 
-  function renderBlog() {
-    blogs_container.innerHTML = "";
-    blogs.map((blog) => {
-      blogs_container.innerHTML += `
+function renderBlog() {
+  blogs_container.innerHTML = "";
+  blogs.map((blog) => {
+    blogs_container.innerHTML += `
       <div class="blog-cont">
               <div class="first">
                   <img src="${blog.userObj.profileUrl}" height="100" width="150" id="profileImage" alt="User Image">
@@ -21,32 +21,26 @@ let blogs = [];
               <button class="seeAll">See all from this user</button>
           </div>
       `;
+  });
+
+  const btn_seeAll = document.querySelectorAll(".seeAll");
+  btn_seeAll.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      console.log(blogs[index].userObj.uid);
+      localStorage.setItem("uid", blogs[index].userObj.uid);
+      window.location = "seeAllBlogs.html";
     });
+  });
+}
 
-    const btn_seeAll = document.querySelectorAll('.seeAll');
-    btn_seeAll.forEach((button, index)=>{
-      button.addEventListener('click', ()=> {
-        console.log(blogs[index].userObj.uid);
-        localStorage.setItem('uid', blogs[index].userObj.uid);
-        window.location = 'seeAllBlogs.html';
-      });
-    });
+async function renderBlogs() {
+  const querySnapshot = await getDocs(collection(db, "blogs"));
+  querySnapshot.forEach((doc) => {
+    blogs.push({ ...doc.data(), docId: doc.id });
+    console.log(blogs);
+    console.log(doc.id, " => ", doc.data());
+    renderBlog();
+  });
+}
 
-
-  }
-  
-
-  async function renderBlogs() {
-    const querySnapshot = await getDocs(collection(db, "blogs"));
-    querySnapshot.forEach((doc) => {
-      blogs.push({ ...doc.data(), docId: doc.id });
-      console.log(blogs);
-      console.log(doc.id, " => ", doc.data());
-      renderBlog();
-    });
-  }
-
-  renderBlogs();
-  
-
-
+renderBlogs();
--- scripts/seeAllBlogs.js
+++ scripts/seeAllBlogs.js
@@ -1,18 +1,49 @@
 import { auth, db } from "../config.js";
 import {
-    collection,
-    getDocs,
-    where, 
-    query,
-  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  collection,
+  getDocs,
+  where,
+  query,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const uid =  localStorage.getItem('uid');
-
-const userName = document.querySelector('.blogs');
-userName.textContent = 'All Blogs from';
-
-
-
-
-
+const uid = localStorage.getItem("uid");
+const blogs_container = document.querySelector(".blogs-container");
+const userEmail = document.querySelector(".userEmail");
+const username = document.querySelector(".username");
+const userImage = document.querySelector(".userImage");
 console.log(uid);
+
+let blogs = [];
+
+function renderBlog() {
+  blogs_container.innerHTML = "";
+  blogs.map((blog) => {
+    blogs_container.innerHTML += `
+    <div class="blog-cont">
+            <div class="first">
+                <img src="${blog.userObj.profileUrl}" height="70" width="100" id="profileImage" alt="User Image">
+                <p class="title">${blog.title}</p>
+            </div>
+            <p>${blog.discription}</p>
+        </div>
+    `;
+  });
+}
+
+async function renderBlogs(userId) {
+  const q = query(collection(db, "blogs"), where("uid", "==", userId));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    blogs.push({ ...doc.data(), docId: doc.id });
+    const userName = document.querySelector(".blogs");
+    userName.textContent = `All from ${doc.data().userObj.firstName} ${doc.data().userObj.lastName}`;
+    userEmail.textContent = `${doc.data().userObj.email}`;
+    username.textContent = `${doc.data().userObj.firstName} ${doc.data().userObj.lastName}`;
+    userImage.src = `${doc.data().userObj.profileUrl}`;
+    console.log(blogs);
+    console.log(doc.id, " => ", doc.data());
+    renderBlog();
+  });
+}
+
+renderBlogs(uid);
